542b5a917405445803f56dc31d0328de9197ef07
eun417 <eunahchung0417@gmail.com>
2023-02-28 15:46:14 +0900

    회원가입 화면

---
+++ semiProject/src/main/java/com/iei/user/controller/JoinFrmServlet.java
@@ -0,0 +1,44 @@
+package com.iei.user.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JoinFrmServlet
+ */
+@WebServlet(name = "JoinFrm", urlPatterns = { "/joinFrm.do" })
+public class JoinFrmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JoinFrmServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//페이지 이동
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/user/joinFrm.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
