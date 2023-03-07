cf09c38f21e4b1b8148a5bdda78d62a0fbd6bbf6
nexsixflux <aac68@naver.com>
2023-03-07 17:18:46 +0900

    신고 게시판 제작

---
+++ semiProject/src/main/java/com/iei/reportManage/controller/EditAnswerViewServlet.java
@@ -0,0 +1,41 @@
+package com.iei.reportManage.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+// 신고 답변 보기 화면
+@WebServlet(name = "EditAnswerView", urlPatterns = { "/question/editAnswerView.do" })
+public class EditAnswerViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public EditAnswerViewServlet() {
+		super();
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		QuestionService service = new QuestionService();
+		
+		String questionNo = request.getParameter("questionNo");
+		
+		QuestionVo result = service.selectOneQuestion(questionNo);
+		
+		request.setAttribute("questionResult", result);
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/question/editAnswerView.jsp");
+		view.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
---
+++ semiProject/src/main/java/com/iei/reportManage/controller/ReportManageWriteServlet.java
@@ -0,0 +1,87 @@
+package com.iei.reportManage.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.iei.reportManage.model.service.ReportManageService;
+import com.iei.reportManage.model.vo.ReportManageVo;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+
+@WebServlet(name = "ReportManageWrite", urlPatterns = { "/reportManageWrite.do" })
+public class ReportManageWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReportManageWriteServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//1 인코딩
+				request.setCharacterEncoding("utf-8");
+				//2 값 추출
+				String root = getServletContext().getRealPath("/");
+				String saveDirectory = root+"upload/reportManage";
+				//2-2. 파일 업로드 최대용량 설정(일반적으로 웹은 10MB)
+				int maxSize = 10*1024*1024;
+				//2-3. multipart/form-data에서 데이터를 꺼내기 위한 객체 변환 작업
+				//request - > MultipartRequest객체로 변환 (cos.jar)
+				//매개변수 5개를 전달하면 변환
+				// 1)request,2)파일저장경로,3)파일최대크기,4)인코딩타입,5)파일중복처리객체
+				MultipartRequest mRequest
+				= new MultipartRequest(request, saveDirectory, maxSize,"UTF-8",new DefaultFileRenamePolicy());
+				//request -> MultipartRequest로 변환 완료
+				//- > 파일업로드가 완료되는 시점
+				// 입력 정보를 추출 (MultipartRequest에서 추출)
+				String reportManageTitle = mRequest.getParameter("reportManageTitle");
+				//제목
+				String reportManageId = mRequest.getParameter("reportManageId");
+				//작성자 아이디 전송
+				String reportManageName = mRequest.getParameter("reportManageName");
+				//작성자 닉네임 전송
+				
+				String reportManageContent = mRequest.getParameter("reportManageContent");
+				//글 전송
+				
+				String filename = mRequest.getOriginalFileName("upfile");
+				//실제로 첨부한 파일 이름 (중복처리 전 파일명)
+				
+				String filepath = mRequest.getFilesystemName("upfile");
+				//실제 서버에 업로드된 파일 이름(중복처리 후 파일명)
+				ReportManageVo r = new ReportManageVo();
+				r.setReportTitle(noticeTitle);
+				r.setNoticeWriter(noticeWriter);
+				r.setNoticeContent(noticeContent);
+				r.setFilename(filename);
+				r.setFilepach(filepath);
+				//3 비즈니스 로직
+				NoticeService service = new NoticeService();
+				int result = service.insertNotice(n);
+				//4 결과 처리
+				RequestDispatcher view =
+						request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+				if(result>0) {
+					request.setAttribute("title", "게시글 작성 성공");
+					request.setAttribute("msg", "게시글이 작성되었씁니다.");
+					request.setAttribute("icon", "success");
+				}else {
+					request.setAttribute("title", "게시글 작성 실패");
+					request.setAttribute("msg", "오류가 발생하였습니다.");
+					request.setAttribute("icon", "error");
+				}
+				request.setAttribute("loc", "/noticeList.do?reqPage=1");
+				view.forward(request, response);
+			}
+			protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				doGet(request, response);
+			}
+
+}
