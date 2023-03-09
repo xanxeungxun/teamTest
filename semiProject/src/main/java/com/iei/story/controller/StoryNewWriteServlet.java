b275867ab398bce8a27862d95dd344992eef3c4b
Janet <janet195@naver.com>
2023-03-09 11:09:07 +0900

    글쓰기,글수정 해결완

---
+++ semiProject/src/main/java/com/iei/story/controller/StoryNewWriteServlet.java
@@ -0,0 +1,79 @@
+package com.iei.story.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.iei.book.model.service.BookService;
+import com.iei.story.model.service.StoryService;
+import com.iei.story.model.vo.Story;
+
+/**
+ * Servlet implementation class storyNewWriteServlet
+ */
+@WebServlet(name = "StoryWrite1", urlPatterns = { "/storyNewWrite.do" })
+public class StoryNewWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StoryNewWriteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1인코딩
+		request.setCharacterEncoding("utf-8");
+		
+		//2값추출
+		String storyName = request.getParameter("storyName");
+		String storyContent = request.getParameter("storyContent");
+		String storyAfter = request.getParameter("storyAfter");
+		Story s = new Story();
+		s.setStoryAfter(storyAfter);
+		s.setStoryContent(storyContent);
+		s.setStoryName(storyName);
+		
+		int bookNo = Integer.parseInt(request.getParameter("bookNo"));
+		
+		//3비즈니스로직
+		StoryService service = new StoryService();
+		int result = service.insertStory(bookNo, s);
+		
+		//4결과처리
+		if(result>0) { //작품 등록 성공
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+			request.setAttribute("title", "작품 등록 완료");
+			request.setAttribute("msg", "작품이 성공적으로 등록되었습니다.");
+			request.setAttribute("icon", "success");
+			request.setAttribute("loc", "/storyList.do?bookNo="+bookNo);
+			view.forward(request, response);
+		}else {//작품 등록 실패
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+			request.setAttribute("title", "작품 수정 실패");
+			request.setAttribute("msg", "오류. 관리자에게 문의하세요");
+			request.setAttribute("icon", "error");
+			request.setAttribute("loc", "/storyList.do?bookNo="+bookNo);
+			view.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- semiProject/src/main/java/com/iei/story/controller/StoryWriteFrmServlet.java
+++ semiProject/src/main/java/com/iei/story/controller/StoryWriteFrmServlet.java
@@ -15,7 +15,7 @@ import com.iei.story.model.vo.Story;
 /**
  * Servlet implementation class StoryWriteFrmServlet
  */
-@WebServlet(name = "StoryWriteFrm1", urlPatterns = { "/storyNewWriteFrm.do" })
+@WebServlet(name = "StoryWriteFrm", urlPatterns = { "/storyWriteFrm.do" })
 public class StoryWriteFrmServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -35,9 +35,10 @@ public class StoryWriteFrmServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		//2값추출
 		int bookNo = Integer.parseInt(request.getParameter("bookNo"));
+		int storyNo = Integer.parseInt(request.getParameter("storyNo"));
 		//3비즈니스로직
 		StoryService service = new StoryService();
-		Story story = service.selectOneStory(bookNo);
+		Story story = service.selectOneStory(storyNo);
 		//4결과처리
 		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/story/storyWriteFrm.jsp");
 		request.setAttribute("bookNo", bookNo);
